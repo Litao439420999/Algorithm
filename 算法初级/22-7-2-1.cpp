1cd372ab4d649989e6dd3fed8fac9b827eef865d
林声飘扬 <54737720+Litao439420999@users.noreply.github.com>
2022-07-02 16:24:21 +0800

    特殊排序
    题目来源：华中科技大学复试上机题
    题目链接：http://t.cn/E9gio39
    描述
    输入一系列整数，将其中最大的数挑出(如果有多个，则挑出一个即可)，并将剩下的数进行排序，如果无剩余的数，则输出-1。
    输入描述：
    输入第一行包括1个整数N，1<=N<=1000，代表输入数据的个数。 接下来的一行有N个整数。
    输出描述：
    可能有多组测试数据，对于每组数据， 第一行输出一个整数，代表N个整数中的最大值，并将此值从数组中去除，将剩下的数进行排序。 第二行将排序的结果输出。

---
+++ 算法初级/22-7-2-1.cpp
@@ -0,0 +1,51 @@
+//并将此值从数组中去除，将剩下的数进行排序。 第二行将排序的结果输出。
+//示例1
+/*输入：
+4
+1 3 4 2
+复制
+输出：
+4
+1 2 3*/
+
+#include <iostream>
+#include <cstdio>
+#include <vector>
+#include <algorithm>
+#include <iterator>
+using namespace std;
+
+vector<int>  numAarry;
+
+int main()
+{
+    int n,num;
+
+    while(scanf("%d",&n)!= EOF)
+    {
+        numAarry.clear();
+        for(int i = 0; i < n; i++)
+        {
+             scanf("%d",&num);
+        
+            numAarry.push_back(num);
+        }   
+  
+
+    vector<int>:: iterator  numOfIterator;
+    sort(numAarry.begin(),numAarry.end());
+    
+    int num1 = numAarry[numAarry.size()-1];
+    numAarry.pop_back();
+    cout << num1 << endl;
+   
+       
+    
+    for(numOfIterator = numAarry.begin(); numOfIterator != numAarry.end();numOfIterator++)
+        cout << *numOfIterator << " " ;
+    }
+    
+    return 0;
+}
+
+
